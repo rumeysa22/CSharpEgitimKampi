246b9a1d79cba75a0e6b04cee536ddd8c18b1890
rumeysa22 <77965395+rumeysa22@users.noreply.github.com>
2024-10-22 15:48:08 +0300

    Add files via upload

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+/*
+ * yazdırma komutları-------------------------------
+ * console.WriteLine("merhaba");     burada line ın sebebi merhabadan sonra alt satıra geçiyor olması
+ * console.Write("canım");           burada sadece yazdrıyor
+ * console.Read();                   ekranda tutuyor
+ * 
+ * değişkenler-------------------------------------
+ * 
+ * 
+ * 
+ * 
+ */
